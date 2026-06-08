495fc16f040135e1ebe3f26af3612c5523c6faa5
supreethragavendra26 <supreethragavendra.s@aximsoft.com>
2026-06-08 19:19:08 +0530

    Day-04: JS

---
+++ Day-04-JS/16-The-Document-Object-Model-(DOM)/16.1 DOM Challenge Starting Files/index.js
@@ -0,0 +1,11 @@
+// Change third element of list
+document.querySelectorAll(".list")[2].innerHTML = "Apple";
+
+// Change color of anchor tag
+document.querySelector(".list a").style.color = "red";
+
+// Change background color of button to yellow
+document.querySelector("button").style.backgroundColor = "yellow";
+
+// Add a class named "huge" to h1
+document.querySelector("h1").classList.add("huge");
