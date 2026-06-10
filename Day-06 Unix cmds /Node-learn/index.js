2ff957b3ca4f3af15504a587e140964305aad48a
supreethragavendra26 <supreethragavendra.s@aximsoft.com>
2026-06-10 20:35:37 +0530

    Add Day 06 Unix cmds

---
+++ Day-06 Unix cmds /Node-learn/index.js
@@ -0,0 +1,10 @@
+console.log("Hello world ");
+console.log("This is my first node js code");
+
+const { generateRandomno, twotables, celtofahren } = require("./utils");
+
+console.log(generateRandomno());
+
+console.log(celtofahren(25));
+
+twotables();
